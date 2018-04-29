9938c9184cc92f223094b07760007f1bd64dac8e
Nicolas Hampton <nicolasjhampton@gmail.com>
2018-04-29 08:26:14 -0700

    Branches

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/Users/nicolashampton/projects/rust/branches/target/debug/branches.dSYM: /Users/nicolashampton/projects/rust/branches/src/main.rs
---
+++ branches/target/debug/deps/branches-b00fd7e3c89d29be.d
@@ -0,0 +1,5 @@
+/Users/nicolashampton/projects/rust/branches/target/debug/deps/branches-b00fd7e3c89d29be: src/main.rs
+
+/Users/nicolashampton/projects/rust/branches/target/debug/deps/branches-b00fd7e3c89d29be.d: src/main.rs
+
+src/main.rs:
