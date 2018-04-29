b3b53edfcc01453cfffb2a105069b10a6ecd531a
Nicolas Hampton <nicolasjhampton@gmail.com>
2018-04-28 19:07:23 -0700

    loops

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,28 +8,28 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng()
-      .gen_range(1, 101); //.to_string();
+    let secret_number = rand::thread_rng().gen_range(1, 101); //.to_string();
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-     // mut indicates a mutable variable
-    let mut guess = String::new();
+        // mut indicates a mutable variable
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-      .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-      .expect("Please type a number!");
-    
-    println!("You guessed: {}", guess);
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    match guess.cmp(&secret_number) {
-      Ordering::Less => println!("Too small!"),
-      Ordering::Greater => println!("Too big!"),
-      Ordering::Equal => println!("You win!"),
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
-
 }
