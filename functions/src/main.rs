42e5a48e1f3353970b8953507d29e1422124949e
Nicolas Hampton <nicolasjhampton@gmail.com>
2018-04-29 07:55:01 -0700

    Finished with functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,7 +2,11 @@ fn main() {
     println!("Hello, world!");
 
     another_function(5, 6);
-    block_expression()
+    block_expression();
+    let z = five();
+    println!("The value of z is: {}", z);
+    println!("The value of add_one(9) is: {}", add_one(9));
+
 }
 
 fn another_function(x: i32, y: i32) {
@@ -20,4 +24,12 @@ fn block_expression() {
 
   println!("The value of a is: {}", a);
   println!("The value of b is: {}", b);
+}
+
+fn add_one(x: i32) -> i32 {
+  x + 1
+}
+
+fn five() -> i32 {
+  5
 }
